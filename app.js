59f8ac6dcd4100f3360633a45beb88e30e9ce8d4
19P602 - Ajith Kumar <ajithkumar11121@gmail.com>
2021-07-20 21:42:02 +0530

    add procfile and create heroku app

--- app.js
+++ app.js
@@ -74,7 +74,11 @@ app.post("/", (req, res) => {
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("server running on port 3000");
 });
