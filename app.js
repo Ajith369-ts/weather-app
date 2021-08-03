9e5515357fb8901c050341013260cfb8b398ce76
19P602 - Ajith Kumar <ajithkumar11121@gmail.com>
2021-08-03 20:13:39 +0530

    Code size updated

--- app.js
+++ app.js
@@ -28,47 +28,18 @@ app.post("/", (req, res) => {
         response.on("data", (d) => {
 
             const weatherData = JSON.parse(d);
-            const climate = weatherData.list[0].weather[0].main;
             const fullName = new Intl.DisplayNames(['en'], { type: 'region' });
             const country = fullName.of(weatherData.city.country);
-            const iconId = weatherData.list[0].weather[0].icon;
-            const iconurl = "./images/weather icons/"+iconId+".svg";
-            const icon1 = "./images/weather icons/"+weatherData.list[3].weather[0].icon+".svg";
-            const icon2 = "./images/weather icons/"+weatherData.list[7].weather[0].icon+".svg";
-            const icon3 = "./images/weather icons/"+weatherData.list[11].weather[0].icon+".svg";
-            const icon4 = "./images/weather icons/"+weatherData.list[15].weather[0].icon+".svg";
+            // const iconurl = "./images/weather icons/"+iconId+".svg";
+            // const icon1 = "./images/weather icons/"+weatherData.list[3].weather[0].icon+".svg";
 
             res.render("index", {
                 date: date(),
-                climate: climate, 
-                discription: weatherData.list[0].weather[0].description, 
+                weatherData: weatherData.list,
                 city: weatherData.city.name,
-                country: country,
-                iconId: iconurl,
-                icon1: icon1,
-                icon2: icon2,
-                icon3: icon3,
-                icon4: icon4,
-                mintemp: weatherData.list[0].main.temp_min,
-                maxtemp: weatherData.list[0].main.temp_max,
-                windSpeed: weatherData.list[0].wind.speed,
-                humidity: weatherData.list[0].main.humidity,
-                temp1: weatherData.list[0].main.temp, 
-                temp2: weatherData.list[3].main.temp,
-                temp3: weatherData.list[7].main.temp,
-                temp4: weatherData.list[11].main.temp,
-                temp5: weatherData.list[15].main.temp,
-                day1: new Date(weatherData.list[3].dt_txt).toDateString().split(' ')[0],
-                day2: new Date(weatherData.list[7].dt_txt).toDateString().split(' ')[0], 
-                day3: new Date(weatherData.list[11].dt_txt).toDateString().split(' ')[0],
-                day4: new Date(weatherData.list[15].dt_txt).toDateString().split(' ')[0],
-                time1: new Date(weatherData.list[3].dt * 1000).toLocaleTimeString("en-US").replace(":00", ""),
-                time2: new Date(weatherData.list[7].dt * 1000).toLocaleTimeString("en-US").replace(":00", ""),
-                time3: new Date(weatherData.list[11].dt * 1000).toLocaleTimeString("en-US").replace(":00", ""),
-                time4: new Date(weatherData.list[15].dt * 1000).toLocaleTimeString("en-US").replace(":00", "")   
-            
+                country: country
+        
             });
-
         })
     });
 
